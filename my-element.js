55854318e5a07142234e05908ed5f5a75d98c7fa
doutori <sasa.hiro0125@gmail.com>
2018-12-19 01:17:29 +0900

    initial commit

---
+++ my-element.js
@@ -0,0 +1,20 @@
+import { LitElement, html } from '@polymer/lit-element';
+
+class CustomGreeting extends LitElement {
+  static get properties() {
+    return {
+      name: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.name = 'World';
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+
+customElements.define('custom-greeting', CustomGreeting);
